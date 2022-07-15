fa7548deab3f85203cafccd5fc5a98b4cb8d5b13
Rubens Zandomenighi Laszlo <rubens.zandom@gmail.com>
2022-07-15 09:19:16 -0300

    Update customers with only special request

--- q10_only_special_ordem.sql
+++ q10_only_special_ordem.sql
@@ -1,39 +1,24 @@
-select
-    COUNT()
-from
-    CUSTOMER
-    LEFT JOIN (
-        SELECT
-            DISTINCT(C_CUSTKEY) AS EXCL_KEYS
-        FROM
-            CUSTOMER
-            LEFT JOIN ORDERS ON ORDERS.O_CUSTKEY = CUSTOMER.C_CUSTKEY
-        where
-            O_COMMENT NOT LIKE '%special request%'
-            OR O_COMMENT LIKE '%unusual package%'
-    ) ON CUSTOMER.C_CUSTKEY = EXCL_KEYS
-WHERE
-    EXCL_KEYS IS NULL;
-
 SELECT
     COUNT(DISTINCT(C_CUSTKEY))
 FROM
     (
         SELECT
-            *
+            C_CUSTKEY
         FROM
-            orders,CUSTOMER
+            CUSTOMER,
+            ORDERS
+        WHERE
+            CUSTOMER.C_CUSTKEY = ORDERS.O_CUSTKEY
+        EXCEPT
+        SELECT
+            C_CUSTKEY
+        FROM
+            orders,
+            CUSTOMER
         WHERE
             customer.c_custkey = orders.o_custkey
-    )
-WHERE
-    NOT EXISTS (
-        SELECT
-            *
-        FROM
-            CUSTOMER
-            LEFT JOIN ORDERS ON ORDERS.O_CUSTKEY = CUSTOMER.C_CUSTKEY
-        where
-            O_COMMENT NOT LIKE '%special request%'
-            OR O_COMMENT LIKE '%unusual package%'
+            AND (
+                O_COMMENT NOT LIKE '%special request%'
+                OR O_COMMENT LIKE '%unusual package%'
+            )
     );
